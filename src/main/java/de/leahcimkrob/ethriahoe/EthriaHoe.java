b53ea3a6cf46d4aecfdaec9d455f4839a8318f64
Bloody_Mind <70509359+Leahcimkrob@users.noreply.github.com>
2025-05-31 12:10:00 +0200

    Update

--- src/main/java/de/leahcimkrob/ethriahoe/EthriaHoe.java
+++ src/main/java/de/leahcimkrob/ethriahoe/EthriaHoe.java
@@ -1,35 +1,16 @@
-package de.leahcimkrob.ethriahoe; // Passe das Package an
+package de.leahcimkrob.ethriahoe;
 
-import org.bukkit.Material;
-import org.bukkit.entity.ItemFrame;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class EthriaHoe implements Listener {
-    private final Material toggleItem = Material.WOODEN_HOE; // Kann bei Bedarf konfigurierbar gemacht werden
+public class EthriaHoe extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Plugin-Start-Logik
+        getServer().getPluginManager().registerEvents(new EthriaHoeListener(), this);
+    }
 
-    @EventHandler(ignoreCancelled = true)
-    public void onToggleFrameProperty(PlayerInteractEntityEvent event) {
-        if (!(event.getRightClicked() instanceof ItemFrame)) return;
-
-        ItemFrame frame = (ItemFrame) event.getRightClicked();
-        Player p = event.getPlayer();
-
-        if (p.getInventory().getItemInMainHand().getType() != toggleItem)
-            return;
-
-        if (p.isSneaking()) {
-            if (p.hasPermission("imagemaps.toggleFixed")) {
-                event.setCancelled(true);
-                frame.setFixed(!frame.isFixed());
-                p.sendMessage("Frame set to " + (frame.isFixed() ? "fixed" : "unfixed"));
-            }
-        } else if (p.hasPermission("imagemaps.toggleVisible")) {
-            event.setCancelled(true);
-            frame.setVisible(!frame.isVisible());
-            p.sendMessage("Frame set to " + (frame.isVisible() ? "visible" : "invisible"));
-        }
+    @Override
+    public void onDisable() {
+        // Plugin-Ende-Logik (optional)
     }
 }
---
+++ src/main/java/de/leahcimkrob/ethriahoe/EthriaHoeListener.java
@@ -0,0 +1,34 @@
+package de.leahcimkrob.ethriahoe;
+
+import org.bukkit.Material;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public class EthriaHoeListener implements Listener {
+    private final Material toggleItem = Material.WOODEN_HOE; // ggf. konfigurierbar
+
+    @EventHandler(ignoreCancelled = true)
+    public void onToggleFrameProperty(PlayerInteractEntityEvent event) {
+        if (!(event.getRightClicked() instanceof ItemFrame)) return;
+        Player p = event.getPlayer();
+        ItemFrame frame = (ItemFrame) event.getRightClicked();
+
+        if (p.getInventory().getItemInMainHand().getType() != toggleItem)
+            return;
+
+        if (p.isSneaking()) {
+            if (p.hasPermission("imagemaps.toggleFixed")) {
+                event.setCancelled(true);
+                frame.setFixed(!frame.isFixed());
+                p.sendMessage("Frame set to " + (frame.isFixed() ? "fixed" : "unfixed"));
+            }
+        } else if (p.hasPermission("imagemaps.toggleVisible")) {
+            event.setCancelled(true);
+            frame.setVisible(!frame.isVisible());
+            p.sendMessage("Frame set to " + (frame.isVisible() ? "visible" : "invisible"));
+        }
+    }
+}
