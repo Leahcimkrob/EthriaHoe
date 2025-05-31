4bb3427c0649d54924c89311490d783e7832023a
Bloody_Mind <70509359+Leahcimkrob@users.noreply.github.com>
2025-05-31 10:29:34 +0200

    Create new project

---
+++ src/main/java/bloody_mind/Ethria_Hoe/EthriaHoe.java
@@ -0,0 +1,35 @@
+package bloody_mind.Ethria_Hoe; // Passe das Package an
+
+import org.bukkit.Material;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public class EthriaHoe implements Listener {
+    private final Material toggleItem = Material.WOODEN_HOE; // Kann bei Bedarf konfigurierbar gemacht werden
+
+    @EventHandler(ignoreCancelled = true)
+    public void onToggleFrameProperty(PlayerInteractEntityEvent event) {
+        if (!(event.getRightClicked() instanceof ItemFrame)) return;
+
+        ItemFrame frame = (ItemFrame) event.getRightClicked();
+        Player p = event.getPlayer();
+
+        if (p.getInventory().getItemInMainHand().getType() != toggleItem)
+            return;
+
+        if (p.isSneaking()) {
+            if (p.hasPermission("imagemaps.toggleFixed")) {
+                event.setCancelled(true);
+                frame.setFixed(!frame.isFixed());
+                p.sendMessage("Frame set to " + (frame.isFixed() ? "fixed" : "unfixed"));
+            }
+        } else if (p.hasPermission("imagemaps.toggleVisible")) {
+            event.setCancelled(true);
+            frame.setVisible(!frame.isVisible());
+            p.sendMessage("Frame set to " + (frame.isVisible() ? "visible" : "invisible"));
+        }
+    }
+}
